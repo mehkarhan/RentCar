e5542d833e6a9deb9683253546dbc9783d31af9b
damirkarahan <damirkarahan@hotmail.com>
2021-02-15 15:03:32 +0300

    15.02.2021

--- Entities/Concrete/Rental.cs
+++ Entities/Concrete/Rental.cs
@@ -5,14 +5,12 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Rent:IEntity
+    public class Rental:IEntity
     {
         public int RentId { get; set; }
         public int CarId { get; set; }
-        public int BrandId { get; set; }
-        public int ColorId { get; set; }
+        public int CustomerId { get; set; }
         public DateTime RentDate { get; set; }
-        
-
+        public DateTime ReturnDate { get; set; }
     }
 }
